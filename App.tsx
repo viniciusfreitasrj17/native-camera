42148af64c8c5c5d0e2ff7c0a5e948af357ea6be
Vinicius Freitas <viniciusfreitasrj17@gmail.com>
2020-07-07 09:38:47 -0300

    Finish App

--- App.tsx
+++ App.tsx
@@ -1,19 +1,126 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { createRef, useState, useEffect } from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  View,
+  Text,
+  TouchableOpacity,
+  Modal,
+  Image,
+} from "react-native";
+import { Camera } from "expo-camera";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function App() {
+  const camRef = createRef<Camera>();
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [hasPermission, setHasPermission] = useState(false);
+  const [capturedPhoto, setCapturedPhoto] = useState("");
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  });
+
+  if (hasPermission === false) {
+    return <Text> Acesso Negado! </Text>;
+  }
+
+  async function takePicture() {
+    if (camRef.current) {
+      const data = await camRef.current.takePictureAsync();
+      setCapturedPhoto(data.uri);
+      setOpen(true);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.androidSaveArea}>
+      <Camera style={{ flex: 1 }} type={type} ref={camRef}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "transparent",
+            flexDirection: "row",
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#35aa99",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: 7,
+              width: 70,
+              height: 50,
+              position: "absolute",
+              bottom: 20,
+              left: 20,
+            }}
+            onPress={() => {
+              setType((type: any) =>
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <Text style={{ fontSize: 18, marginBottom: 13, color: "#fff" }}>
+              Trocar
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+
+      <TouchableOpacity
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#121212",
+          margin: 20,
+          borderRadius: 10,
+          height: 50,
+        }}
+        onPress={takePicture}
+      >
+        <FontAwesome name="camera" size={23} color="#fff" />
+      </TouchableOpacity>
+
+      {!!capturedPhoto && (
+        <Modal animationType="slide" transparent={false} visible={open}>
+          <View
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              margin: 20,
+            }}
+          >
+            <TouchableOpacity
+              style={{ margin: 10 }}
+              onPress={() => setOpen(false)}
+            >
+              <FontAwesome name="window-close" size={50} color="#f00" />
+            </TouchableOpacity>
+
+            <Image
+              style={{ width: "100%", height: 300, borderRadius: 20 }}
+              source={{ uri: capturedPhoto }}
+            />
+          </View>
+        </Modal>
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  androidSaveArea: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
   },
 });
